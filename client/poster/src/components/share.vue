7850d507bc6c51117f21dcb8e4b828cf308a09b4
Seda <sedanurgfb6671@hotmail.com>
2023-07-12 14:15:43 +0300

    added linkedin api login

--- client/poster/src/components/share.vue
+++ client/poster/src/components/share.vue
@@ -5,30 +5,23 @@
         <br>
         <div>
             <center>
-                <button @click="getPost()" class="btn btn-secondary" style="font-size: 20px;" type="submit"> LinkedIn <i
-                        class="fa fa-linkedin-square"></i></button>
+                <button @click="redirectToLinkedIn" class="btn btn-secondary" style="font-size: 20px;" type="submit">
+                    LinkedIn <i class="fa fa-linkedin-square"></i></button>
             </center>
         </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
 export default {
     name: "Share",
     data() {
         return {};
     },
     methods: {
-        async getPost() {
-            axios.post('http://127.0.0.1:3000/post')
-                .then(response => {
-                    console.log('Post başarıyla gönderildi:', response.data);
-                })
-                .catch(error => {
-                    console.error('Post gönderme hatası:', error);
-                });
-        }
+        redirectToLinkedIn() {
+            window.location.href = "http://127.0.0.1:3000/login";
+        },
     }
 }
 </script>
--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
 const axios = require("axios");
 
+const querystring = require("querystring");
+
 // CORS
 app.use(cors());
 
@@ -58,48 +60,158 @@ app.post("/generate", async (req, res) => {
 
 //linkedin part
 
-linkedin_Api = process.env.LINKEDIN_API_KEY;
-linkedin_user_id = process.env.LINKEDIN_ID_KEY;
+LINKEDIN_API_KEY = process.env.LINKEDIN_API_KEY;
+LINKEDIN_ID_KEY = process.env.LINKEDIN_ID_KEY;
 
-app.post("/post", async () => {
-  console.log("post");
-  // try {
-  //   const accessToken = `Bearer ${linkedin_Api}`;
+// LinkedIn API kimlik bilgileri
+const clientID = LINKEDIN_ID_KEY;
+const clientSecret = LINKEDIN_API_KEY;
+const callbackURL = "http://localhost:3000/callback";
 
-  //   const postContent = {
-  //     author: `f'urn:li:person:${linkedin_user_id}`, // Gönderiyi oluşturan kişinin LinkedIn kullanıcı kimliği
-  //     lifecycleState: "PUBLISHED",
-  //     specificContent: {
-  //       "com.linkedin.ugc.ShareContent": {
-  //         shareCommentary: {
-  //           text: "Bu gönderi LinkedIn üzerinden paylaşıldı.", // Gönderi metni
-  //         },
-  //         shareMediaCategory: "NONE",
-  //       },
-  //     },
-  //     visibility: {
-  //       "com.linkedin.ugc.MemberNetworkVisibility": "CONNECTIONS", // Gönderinin görünürlük ayarı (CONNECTIONS, PUBLIC)
-  //     },
-  //   };
+const session = require("express-session");
+const passport = require("passport");
+const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
 
-  //   const response = await axios.post(
-  //     "https://api.linkedin.com/v2/ugcPosts",
-  //     postContent,
-  //     {
-  //       headers: {
-  //         Authorization: accessToken,
-  //         "Content-Type": "application/json",
-  //       },
-  //     }
-  //   );
-
-  //   console.log(response.data); // Gönderi oluşturulduğunda alınan yanıtı konsola yazdırabilirsiniz.
-
-  //   res.status(200).json({ message: "Gönderi başarıyla oluşturuldu." });
-  // } catch (error) {
-  //   console.error(error);
-  //   response
-  //     .status(500)
-  //     .json({ message: "Gönderi oluşturulurken bir hata oluştu." });
-  // }
+app.set("view engine", "ejs");
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: clientSecret,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
 });
+passport.deserializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.use(
+  new LinkedInStrategy(
+    {
+      clientID: clientID,
+      clientSecret: clientSecret,
+      callbackURL: callbackURL,
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      const user = {
+        id: profile.id,
+        name: profile.name,
+        email: profile.emailAddress,
+      };
+      cb(null, user);
+    }
+  )
+);
+
+app.get("/", function (req, res) {
+  if (req.user) {
+    res.redirect("/post");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/login", passport.authenticate("linkedin"));
+
+app.get(
+  "/callback",
+  passport.authenticate("linkedin", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
+// app.get("/post", function (req, res) {
+//   if (req.user) {
+//     res.render("post", { user: req.user });
+//   } else {
+//     res.redirect("/login");
+//   }
+// });
+
+app.post("/post", function (req, res) {
+  if (!req.user) {
+    res.redirect("/login");
+  }
+  const postData = {
+    title: req.body.title,
+    body: req.body.body,
+  };
+  const accessToken = req.user.accessToken;
+  const request = require("request");
+  request.post(
+    "https://api.linkedin.com/v2/posts",
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(postData),
+    },
+    function (err, response, body) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(body);
+      }
+    }
+  );
+});
+
+// // Oturum açma isteği
+// app.get("/login", (req, res) => {
+//   console.log("login");
+//   const params = {
+//     response_type: "code",
+//     client_id: clientId,
+//     redirect_uri: redirectUri,
+//     scope: "r_liteprofile w_member_social",
+//   };
+//   const authUrl = `https://www.linkedin.com/oauth/v2/authorization?${querystring.stringify(
+//     params
+//   )}`;
+//   res.redirect(authUrl);
+// });
+
+// // Geri çağrı işlemi
+// app.get("/callback", async (req, res) => {
+//   const code = req.query.code;
+//   console.log("callback");
+
+//   // Erişim token'ını alma isteği
+//   const tokenParams = {
+//     grant_type: "authorization_code",
+//     code: code,
+//     client_id: clientId,
+//     client_secret: clientSecret,
+//     redirect_uri: redirectUri,
+//   };
+//   const tokenResponse = await axios.post(
+//     "https://www.linkedin.com/oauth/v2/accessToken",
+//     querystring.stringify(tokenParams)
+//   );
+//   const accessToken = tokenResponse.data.access_token;
+
+//   // Paylaşım yapma isteği
+//   const shareData = {
+//     comment: "Hello, world!",
+//     visibility: "PUBLIC", // Paylaşımın herkese açık olmasını sağlar
+//   };
+//   console.log("share");
+//   const shareResponse = await axios.post(
+//     "https://api.linkedin.com/v2/ugcPosts",
+//     shareData,
+//     {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//         "Content-Type": "application/json",
+//         "X-Restli-Protocol-Version": "2.0.0",
+//       },
+//     }
+//   );
+
+//   res.send("Paylaşım yapıldı.");
+// });
